12183e03055a456be51179fde0a3888ac25e3b5a
Mathis <thesylverofficial@gmail.com>
2025-04-14 09:40:38 +0200

    refonte du projet

---
+++ backend/db.js
@@ -0,0 +1,20 @@
+// backend/db.js
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion MySQL ❌:', err);
+    return;
+  }
+  console.log('Connexion MySQL réussie ✅');
+});
+
+module.exports = db;
